2ef0eeffb8830e80ee133d78822b3a2199d9bb1a
Lacapuyol5 <fabiantoby80@gmail.com>
2024-10-25 19:54:39 +0100

    almost done

--- techCrush/app.js
+++ techCrush/app.js
@@ -1,11 +1,19 @@
-const path =  require('path');
-const addition = require('./my_modules/calculator');
-const substraction = require('./my_modules/calculator');
-const division  = require('./my_modules/calculator');
-const multiplication  = require('./my_modules/calculator');
+const { path } =  require("path");
+const { addition } = require("./my_modules/calculator");
+const { substraction } = require("./my_modules/calculator");
+const { division  } = require("./my_modules/calculator");
+const { multiplication }  = require("./my_modules/calculator");
 
 //base name of current file
 
-const sum  = addition(2, 2);
+let sum  = addition(2, 2);
 console.log(sum);
 
+let difference  = substraction(4, 8);
+console.log(difference);
+
+let multiples = multiplication(3, 3);
+console.log(multiples);
+
+let divide = division(2, 10);
+console.log(division);
--- techCrush/my_modules/calculator.js
+++ techCrush/my_modules/calculator.js
@@ -23,5 +23,5 @@ module.exports = {
 addition,
 substraction,
   multiplication,
-  division,
+  division
 };
